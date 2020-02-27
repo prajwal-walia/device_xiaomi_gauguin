ad605c20d5d46bffe814ce53883a06fa7e1f978c
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    gauguin: Panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x00000000
