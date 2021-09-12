55278760665512c22873bc56639ce8810ed9f3c3
Arian <arian.kulmer@web.de>
2021-09-12 19:29:17 +0200

    gauguin: rootdir: Drop redundant turing off of scheduler boost
    Change-Id: I354bd6500d053a313bfff2abb2ec205cdfa381c7

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -75,9 +75,6 @@ case "$target" in
 	# Disable wsf for all targets beacause we are using efk.
 	# wsf Range : 1..1000 So set to bare minimum value 1.
 	echo 1 > /proc/sys/vm/watermark_scale_factor
-
-        # Turn off scheduler boost at the end
-        echo 0 > /proc/sys/kernel/sched_boost
       ;;
     esac
 esac
