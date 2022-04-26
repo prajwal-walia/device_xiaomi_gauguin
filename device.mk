917424139203af404db130b51d36534e98866704
Wei Wang <wvw@google.com>
2022-04-25 20:56:35 -0400

    gauguin: Drop QTI perfd stack
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- device.mk
+++ device.mk
@@ -406,9 +406,6 @@ PRODUCT_PACKAGES += \
     resize2fs_ramdisk
 
 # Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
