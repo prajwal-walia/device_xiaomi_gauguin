2bba06fd775476310227c1c7976f7a176b3d5268
Sebastiano Barezzi <seba@sebaubuntu.dev>
2022-05-01 01:26:49 +0200

    gauguin: Move to common Xiaomi fingerprint HIDL
    Change-Id: I070ab965ff932f71f2ffbd4d7986752295810726

--- device.mk
+++ device.mk
@@ -215,7 +215,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_gauguin
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
