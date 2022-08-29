3564df3f1a3cfb175da717c8239ea735e619bffb
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    gauguin: Switch to legacy sepolicy_vndr
     - Also define TARGET_SEPOLICY_DIR as "lito"
    
    Change-Id: I738c4f0c384d0c913d2202ec7899ead173376722

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-07-01
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+TARGET_SEPOLICY_DIR := lito
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
