bd6062a232052e58333e37abc8d47b51880ec59d
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-07 20:09:45 +0000

    gauguin: Build android.hardware.bluetooth.audio@2.1
    [1] - It seems that for qti bt this should still be built with AIDL, to avoid problems with bt
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I06c581c72a9f0f99331049740af934d96a0b59a7

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ BOARD_HAVE_QCOM_FM := false
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
