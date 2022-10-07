eb333d1a2d6e17c9d13a550ff8644bcac2780302
johnmart19 <johnivan19999@gmail.com>
2022-10-08 02:13:09 +0300

    gauguin: Build legacy android.frameworks.sensorservice@1.0
    [   19.866353] init: Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 637 (/system/bin/hwservicemanager)
    
    Change-Id: I51cb96361bb7fc2aa4729b1689dd577229fe587b

--- device.mk
+++ device.mk
@@ -469,7 +469,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
