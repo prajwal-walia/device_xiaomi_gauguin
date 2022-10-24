6e181434d9e6101bd84c6430b078d870becf8353
kawaaii <kawaaii@nocturn9x.space>
2022-10-24 17:39:04 +0545

    gauguin: Revert back to fstab.{ default emmc }
     - Follow OEM's way
    
    Revert "gauguin: rootdir: fstabs: Migrate to fstab.qcom"
    
    This reverts commit fb4fc3e24e327fc4beb7d39953d190bc6c5e2497.
    
    Revert "gauguin: Enforce fstab suffix to qcom"
    
    This reverts commit cbb34f3020741ff23f92021824b150954e053b84.
    
    Change-Id: I43fdc4b30426a36a3f5e85223f94a3e1e8c15e11

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
@@ -152,7 +151,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.emmc
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
