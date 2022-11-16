3bbefc98b823e6dee6f39d8041db17d8a058483b
hridaya <hridaya@pixelos.net>
2022-11-16 19:14:32 +0545

    gauguin: Call QTI USB init scripts from source
    Signed-off-by: hridaya <hridaya@pixelos.net>
    Change-Id: Ibaa54fcf9e47beb6c4c06bc70beac0e44c0fc4c4

--- device.mk
+++ device.mk
@@ -502,8 +502,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # USB
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
+
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
