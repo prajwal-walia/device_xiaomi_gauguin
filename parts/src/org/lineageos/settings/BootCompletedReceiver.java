916e2526679089582b5e7791ede3a446b0a937d8
hridaya <hridaya@pixelos.net>
2022-11-23 08:28:44 +0545

    gauguin: Drop Xiaomi Doze
    Signed-off-by: hridaya <hridaya@pixelos.net>
    Change-Id: I0e323c82ce4f652fce495264384a882a6c10c4d4

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -40,6 +39,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
-        DozeUtils.checkDozeService(context);
     }
 }
