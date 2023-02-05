4a19dd99f337e6f413a0ef10738fe0450d67143b
hridaya <hridaya@pixelos.net>
2023-02-05 12:44:47 +0545

    Revert "gauguin: Switch BtAudio to AIDL"
     - also add missing manifest for hidl bt
    
    This reverts commit ec11f3a5014510156713d4817987b7b2eca1e69f.
    
    Ref: https://review.lineageos.org/c/LineageOS/android_device_oneplus_sm8350-common/+/338968
    
    Change-Id: Ifb3560cccfccdc2ec69b174cdccecb96368c51fe

--- device.mk
+++ device.mk
@@ -133,7 +133,6 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
