2bad7531728fc76a600df95ca75a750ce19c5f56
hridaya <hridaya@pixelos.net>
2023-03-18 12:32:01 +0545

    gauguin: Don't build non exist `libldacBT_*` libraries
    - These libraries don't have build system as seen in [1], but they exist in the framework of AOSP as of [2] and [3].
    
    [1]: https://cs.android.com/android/platform/superproject/+/master:packages/modules/Bluetooth/system/stack/Android.bp
    [2]: libldacBT_dec {"https://cs.android.com/android/platform/superproject/+/master:packages/modules/Bluetooth/system/stack/test/stack_a2dp_test.cc;l=253?q=libldacBT_dec"}
    [3]: libldacBT_bco {"https://cs.android.com/android/platform/superproject/+/master:packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc;l=48?q=libldacBT_bco"}
    
    Change-Id: I80469eeafbea4773159a67e5526c1c3d7aff898f

--- device.mk
+++ device.mk
@@ -140,10 +140,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbthost_if.vendor \
     libldacBT_enc \
-    libldacBT_dec \
-    libldacBT_abr \
-    libldacBT_bco \
-    libldacBT_bco.vendor
+    libldacBT_abr
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
