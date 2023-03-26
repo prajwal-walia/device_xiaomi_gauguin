cf646c5976cfb1e4eca6ca9277f7b88f4970ad6b
hridaya <hridaya@pixelos.net>
2023-03-26 19:37:42 +0545

    Revert "gauguin: Build legacy android.frameworks.sensorservice@1.0"
    03-26 14:31:48.825   603   603 I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    03-26 14:31:48.827   603   603 I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    03-26 14:31:48.828   983  1885 I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    03-26 14:31:48.832   603  1888 W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    03-26 14:31:48.832   603  1888 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    03-26 14:31:48.840   603  1887 W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    03-26 14:31:48.840   603  1887 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    
    This reverts commit eb333d1a2d6e17c9d13a550ff8644bcac2780302.

--- device.mk
+++ device.mk
@@ -449,8 +449,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
-    android.frameworks.sensorservice@1.0
+    android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
